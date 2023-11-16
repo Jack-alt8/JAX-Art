8f9fba30266a0305e5465d902775f0647b08bcc7
Jack Baylor <jackc.baylor@gmail.com>
2023-11-16 12:07:44 -0500

    cloudinary upload api

--- backend/server/index.js
+++ backend/server/index.js
@@ -7,3 +7,13 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
+
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+    cloud_name: 'dnltdvkyt',
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true
+});
+
+cloudinary.uploader.upload
