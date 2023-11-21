b623930c4f0dcf714576bd3e4bfbff1808a64a12
Jack Baylor <jackc.baylor@gmail.com>
2023-11-21 14:08:24 -0500

    minor fixes for api call

--- backend/server/index.js
+++ backend/server/index.js
@@ -6,15 +6,20 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { send } = require("process");
 require("dotenv").config();
 
+// request button or robot generator
+const request = document.getElementById("request-button");
+request.addEventListener("click", sendGetRequest);
+
 // retrieve api and display
-    const input = document.getElementbyId("robohash-input");
+    const input = document.getElementById("robohash-input");
     const img = document.createElement('img');
-    const url = `https://robohash.org/${input}?size=200x200`;
+    const url = `https://robohash.org/${input}?anysize=200x200`;
     fetch(url)
         .then((response) => response.blob())
         .then((blob) => {
-            img.src =url.createObjectURL(blob);
+            img.src = url.createObjectURL(blob);
             document.body.appendChild(img);
         });
