2ad80a9e6a9962bfbae82334eed53d10b077aa70
Jack Baylor <jackc.baylor@gmail.com>
2023-11-21 17:16:07 -0500

    more gallery code

---
+++ frontend/javascript/art.js
@@ -0,0 +1,23 @@
+//turning on strict mode
+"use strict";
+
+// artlist array
+const myArray = [
+  {
+    "../../backend/artimages/Abstract_art/RenderedImage(3).jpg",
+    title: "Untitled_1",
+    size: "100px by 100px",
+    arttype: "Digital Art",
+    Datecreated: "2023",
+    Listenedto: "",
+    Series: "Untitled Series",
+  },
+  {
+    title: "Untitled_2",
+    size: "100px by 100px",
+    arttype: "Digital Art",
+    Datecreated: "2023",
+    Listenedto: "",
+    Series: "Untitled Series",
+  },
+];
--- frontend/javascript/search.js
+++ frontend/javascript/search.js
@@ -1,28 +1,20 @@
 //turning on strict mode
 "use strict";
 
-// artlist array
-const myArray = [
-  {
-    image: buildImage("../../backend/artimages/Abstract_art/RenderedImage(3).jpg"),
-    title: "Untitled_1",
-    size: "100px by 100px",
-    arttype: "Digital Art",
-    Datecreated: "2023",
-    Listenedto: "",
-    Series: "Untitled Series",
-  },
-  {
-    title: "Untitled_2",
-    size: "100px by 100px",
-    arttype: "Digital Art",
-    Datecreated: "2023",
-    Listenedto: "",
-    Series: "Untitled Series",
-  }
-];
-
 const searchBar = getElementById("searchbar");
+let artList = [];
+searchString = "";
+
+fetch("art.js")
+  .then((res) => res.json())
+  .then((data) => {
+    artList = data;
+  });
+
+function findArtListMatch(searchString) {
+  const matchingArt = artList.filter((art) =>
+  art.name.toLowerCase)
+}
 
 const searchButton = getElementById("searchbutton");
 searchButton.addEventListener("keyUp", (e) => {
